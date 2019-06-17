677200c8d2299ca77cfaeeb33c1b218249babdf5
pentondb <derek.penton@gmail.com>
2019-06-16 21:59:09 -0500

    Initial commit (after working through too many units)

---
+++ SchoolManagement/Models/MetaClasses/CoursesMetaData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class CoursesMetaData
+    {
+        /**
+         Changed the namespace from SchoolManagement.Models.MetaClasses to SchoolManagement.Models, so that the partial class will be associated with the full class in the same namespace.
+         */
+        /**
+         * Creating a meta-data class to be related to the Course.cs class in the EDMX-related folder,
+         * so that the data annotations applied in this meta-data class are not wiped out when the database model is refreshed from the database.
+         */
+        [Required, StringLength(50)]
+        public string Title { get; set; }
+
+        [Required, Range(1, 8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetaData))]
+    public partial class Course
+    {
+
+    }
+}
---
+++ SchoolManagement/Models/MetaClasses/StudentsMetaData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class StudentsMetaData
+    {
+        [Required, StringLength(50), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, StringLength(50), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50), Display(Name = "Last Name")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+
+        [Display(Name = "Date of Enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+    }
+
+    [MetadataType(typeof(StudentsMetaData))]
+    public partial class Student
+    {
+
+    }
+}
