0f4b344007d2f26da55ecc409434a4790cb7ea7e
pentondb <derek.penton@gmail.com>
2019-06-17 20:30:07 -0500

    Added Lecturer table and CRUD operations, added foreign key for Lecturer to Enrollment

---
+++ SchoolManagement/Models/MetaClasses/LecturersMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class LecturersMetaData
+    {
+        [StringLength(50), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+
+    [MetadataType(typeof(LecturersMetaData))]
+    public partial class Lecturers
+    {
+
+    }
+}
