452b92a02edffc0344fdf40a91743a64fac473e3
pentondb <derek.penton@gmail.com>
2019-06-17 20:57:35 -0500

    Added jQueryUI

--- SchoolManagement/App_Start/BundleConfig.cs
+++ SchoolManagement/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace SchoolManagement
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +27,8 @@ namespace SchoolManagement
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/themes/base/autocomplete.css"));
         }
     }
 }
